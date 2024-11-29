2c8f16337bd507abd43c909005f93877ff94994e
Nachiket Subbaraman <nachocheeeze922@gmail.com>
2024-11-29 14:15:18 -0800

    Changed PointCloudAutoencoder architecture (& documented changes)

--- AutoEncoder/PointCloudAutoencoder.py
+++ AutoEncoder/PointCloudAutoencoder.py
@@ -16,8 +16,11 @@ class PointCloudAutoEncoder(nn.Module):
         self.latent_size = latent_size
         self.point_size = point_size
         
-        self.conv1 = torch.nn.Conv1d(3, 64, 1)
-        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        # Archictectures I trained: 
+        # 1. Changed conv1 to (3, 64, 1). Kept Conv2 same as PointCloudAE repo - (64, 128, 1). 
+        # 2. Changed conv1 to (6, 64, 1). Changed Conv2 to (64, 128, 2).
+        self.conv1 = torch.nn.Conv1d(6, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 2)
         self.conv3 = torch.nn.Conv1d(128, self.latent_size, 1)
         self.bn1 = nn.BatchNorm1d(64)
         self.bn2 = nn.BatchNorm1d(128)
@@ -28,17 +31,25 @@ class PointCloudAutoEncoder(nn.Module):
         self.dec3 = nn.Linear(256,self.point_size*3)
 
     def encoder(self, x): 
+        # print(f'input: {x.shape}')
         x = F.relu(self.bn1(self.conv1(x)))
+        # print(f'relu(bn1(conv1)): {x.shape}')
         x = F.relu(self.bn2(self.conv2(x)))
+        # print(f'relu(bn2(conv2)): {x.shape}')
         x = self.bn3(self.conv3(x))
+        # print(f'bn3: {x.shape}')
         x = torch.max(x, 2, keepdim=True)[0]
+        # print('max: {x.shape}')
         x = x.view(-1, self.latent_size)
+        # print(f'end of enc x.shape: {x.shape}')
         return x
     
     def decoder(self, x):
+        # print(f'start dec x.shape: {x.shape}')
         x = F.relu(self.dec1(x))
         x = F.relu(self.dec2(x))
         x = self.dec3(x)
+        # print(f'end dec x.shape: {x.shape}')
         return x.view(-1, self.point_size, 3)
     
     def forward(self, x):
--- AutoEncoder/auto_encoder_train_one_point_cloud.py
+++ AutoEncoder/auto_encoder_train_one_point_cloud.py
@@ -2,13 +2,17 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 import Helpers.PointCloudOpen3d as pc
-from PointCloudAutoencoder import PointCloudAE
+from old_auto_encoders import PointCloudAutoencoder
 import numpy as np
 
 
 
-file_path = "../ModelNet40/cup/train/cup_0001.off" 
+file_path = "../../ModelNet40/cup/train/cup_0001.off" 
 num_vertices, vertices = pc.get_off_vertices(file_path)
 # Convert vertices to a NumPy array
 # TODO: Add padding to point clouds with less vertices less than input_dim
@@ -19,7 +23,7 @@ input_dim = num_vertices  # TODO: Change to some constant (e.g. 2000)
 hidden_dim = 128 # TODO: Maximize hidden dim but ensure reasonable training time
 latent_dim = 32
 
-model = PointCloudAE(input_dim, hidden_dim, latent_dim)
+model = PointCloudAutoencoder(input_dim, hidden_dim, latent_dim)
 
 # Create a DataLoader
 batch_size = 32
@@ -41,3 +45,10 @@ for epoch in range(num_epochs):
         optimizer.step()
     
     print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+file_path = "../../ModelNet40/cup/train/cup_0001.off"  # Replace with your .off file path
+num_vertices, vertices = pc.get_off_vertices(file_path)
+point_cloud = pc.get_point_cloud(vertices)
+
+# Visualize the point cloud
+pc.visualize_point_cloud(point_cloud) 

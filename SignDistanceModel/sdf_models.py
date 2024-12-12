ba3574a22db210a5d0ca58f9334aea2d94bd992c
Zach <z.fogg1234@gmail.com>
2024-12-11 21:54:29 -0800

    Collecting results

--- SignDistanceModel/sdf_models.py
+++ SignDistanceModel/sdf_models.py
@@ -3,123 +3,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-
-#####################################
-#######   REGRESSION MODELS  ########
-#####################################
-class SDFRegressionModel_3L(nn.Module):
-    def __init__(self, input_dim, latent_dim, hidden_dim):
-        super(SDFRegressionModel_3L, self).__init__()
-        self.fc1 = nn.Linear(input_dim + latent_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, 1)
-        self.relu = nn.ReLU()
-
-    def forward(self, x, latent):
-        x = torch.cat((x, latent), dim=2)
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-    
-
-class SDFRegressionModel_5L(nn.Module):
-    def __init__(self, input_dim, latent_dim, hidden_dim):
-        super(SDFRegressionModel_5L, self).__init__()
-        # Increase point representation from 3 to 64
-        self.fc0 = nn.Linear(input_dim, 32)
-        self.fc1 = nn.Linear(32, 64)
-
-        # Concat with latent dim to predict SDF value
-        self.fc2 = nn.Linear(64 + latent_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc4 = nn.Linear(hidden_dim, 1)
-        self.relu = nn.ReLU()
-
-    def forward(self, x, latent):
-        x = self.relu(self.fc0(x))
-        x = self.relu(self.fc1(x))
-        x = torch.cat((x, latent), dim=2)
-        x = self.relu(self.fc2(x))
-        x = self.relu(self.fc3(x))
-        x = self.fc4(x)
-        return x
-    
-
-class SDFRegressionModel_7L(nn.Module):
-    def __init__(self, input_dim, latent_dim, hidden_dim):
-        super(SDFRegressionModel_7L, self).__init__()
-        self.fc0 = nn.Linear(input_dim, 32)
-        self.fc1 = nn.Linear(32, 64)
-        self.fc2 = nn.Linear(64, 128)
-
-        self.fc3 = nn.Linear(128 + latent_dim, hidden_dim)
-        self.fc4 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc5 = nn.Linear(hidden_dim, 1)
-        self.relu = nn.ReLU()
-
-    def forward(self, x, latent):
-        x = self.relu(self.fc0(x))
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-
-        x = torch.cat((x, latent), dim=2)
-        x = self.relu(self.fc3(x))
-        x = self.relu(self.fc4(x))
-        x = self.fc5(x)
-        return x
-    
-# Extra Downsampling layer
-class SDFRegressionModel_9L(nn.Module):
-    def __init__(self, input_dim, latent_dim, hidden_dim):
-        super(SDFRegressionModel_9L, self).__init__()
-        self.fc0 = nn.Linear(input_dim, 16)
-        self.fc1 = nn.Linear(16, 32)
-        self.fc2 = nn.Linear(32, 64)
-        self.fc3 = nn.Linear(64, 128)
-
-        self.fc4 = nn.Linear(128 + latent_dim, hidden_dim)
-        self.fc5 = nn.Linear(hidden_dim, hidden_dim) 
-        self.fc6 = nn.Linear(hidden_dim, int(hidden_dim/2)) # (256,128)
-        self.fc7 = nn.Linear(int(hidden_dim/2), 1)
-        self.relu = nn.ReLU()
-
-    def forward(self, x, latent):
-        x = self.relu(self.fc0(x))
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.relu(self.fc3(x))
-
-        x = torch.cat((x, latent), dim=2)
-        x = self.relu(self.fc4(x))
-        x = self.relu(self.fc5(x))
-        x = self.relu(self.fc6(x))
-        x = self.fc7(x)
-        return x
-
-
 ############################################
 ######  BINARY CLASSIFICATION MODELS  ######
 ############################################
-class SDFClassificationModel_3L(nn.Module):
-    def __init__(self, input_dim, latent_dim, hidden_dim):
-        super(SDFClassificationModel_3L, self).__init__()
+class SD_3L(nn.Module):
+    def __init__(self, latent_dim, input_dim = 3, hidden_dim = 256):
+        super(SD_3L, self).__init__()
         self.fc1 = nn.Linear(input_dim + latent_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
         self.fc3 = nn.Linear(hidden_dim, 1)
         self.relu = nn.ReLU()
 
-    def forward(self, x, latent):
-        x = torch.cat((x, latent), dim=2)
+    def forward(self, latent, x ):
+        x = torch.cat((x, latent), dim=1)
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         x = self.fc3(x)
         return x
     
 
-class SDFClassificationModel_5L(nn.Module):
-    def __init__(self, input_dim, latent_dim, hidden_dim):
-        super(SDFClassificationModel_5L, self).__init__()
+class SD_3L_Upscale32(nn.Module):
+    def __init__(self, latent_dim, input_dim = 3, hidden_dim = 256):
+        super(SD_3L_Upscale32, self).__init__()
         # Increase point representation from 3 to 64
         self.fc0 = nn.Linear(input_dim, 32)
         self.fc1 = nn.Linear(32, 64)
@@ -130,19 +35,19 @@ class SDFClassificationModel_5L(nn.Module):
         self.fc4 = nn.Linear(hidden_dim, 1)
         self.relu = nn.ReLU()
 
-    def forward(self, x, latent):
+    def forward(self, latent, x):
         x = self.relu(self.fc0(x))
         x = self.relu(self.fc1(x))
-        x = torch.cat((x, latent), dim=2)
+        x = torch.cat((x, latent), dim=1)
         x = self.relu(self.fc2(x))
         x = self.relu(self.fc3(x))
         x = self.fc4(x)
         return x
     
 
-class SDFClassificationModel_7L(nn.Module):
-    def __init__(self, input_dim, latent_dim, hidden_dim):
-        super(SDFClassificationModel_7L, self).__init__()
+class SD_7L_Upscale128(nn.Module):
+    def __init__(self, latent_dim, input_dim = 3, hidden_dim = 256):
+        super(SD_7L_Upscale128, self).__init__()
         self.fc0 = nn.Linear(input_dim, 32)
         self.fc1 = nn.Linear(32, 64)
         self.fc2 = nn.Linear(64, 128)
@@ -152,21 +57,21 @@ class SDFClassificationModel_7L(nn.Module):
         self.fc5 = nn.Linear(hidden_dim, 1)
         self.relu = nn.ReLU()
 
-    def forward(self, x, latent):
+    def forward(self, latent, x):
         x = self.relu(self.fc0(x))
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
 
-        x = torch.cat((x, latent), dim=2)
+        x = torch.cat((x, latent), dim=1)
         x = self.relu(self.fc3(x))
         x = self.relu(self.fc4(x))
         x = self.fc5(x)
         return x
     
 # Extra Downsampling layer
-class SDFClassificationModel_9L(nn.Module):
-    def __init__(self, input_dim, latent_dim, hidden_dim):
-        super(SDFClassificationModel_9L, self).__init__()
+class SD_4L_Upscale128(nn.Module):
+    def __init__(self, latent_dim, input_dim = 3, hidden_dim = 256):
+        super(SD_4L_Upscale128, self).__init__()
         self.fc0 = nn.Linear(input_dim, 16)
         self.fc1 = nn.Linear(16, 32)
         self.fc2 = nn.Linear(32, 64)
@@ -178,15 +83,126 @@ class SDFClassificationModel_9L(nn.Module):
         self.fc7 = nn.Linear(int(hidden_dim/2), 1)
         self.relu = nn.ReLU()
 
-    def forward(self, x, latent):
+    def forward(self, latent, x):
         x = self.relu(self.fc0(x))
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         x = self.relu(self.fc3(x))
 
-        x = torch.cat((x, latent), dim=2)
+        x = torch.cat((x, latent), dim=1)
         x = self.relu(self.fc4(x))
         x = self.relu(self.fc5(x))
         x = self.relu(self.fc6(x))
         x = self.fc7(x)
+        return x
+    
+
+class SD_5L_Upscale256(nn.Module):
+
+    def __init__(self, latent_rep_size):
+        
+        super().__init__()
+
+        self.up1 = nn.Linear(3, 64)
+        self.up2 = nn.Linear(64, latent_rep_size)
+
+        self.l1 = nn.Linear(latent_rep_size * 2, 1024)
+        self.l2 = nn.Linear(1024, 512)
+        self.l3 = nn.Linear(512, 256)
+        self.l4 = nn.Linear(256, 128)
+        self.l5 = nn.Linear(128, 1)
+
+        self.up_bn1 = nn.BatchNorm1d(64)
+        
+        self.l_bn1 = nn.BatchNorm1d(1024)
+        self.l_bn2 = nn.BatchNorm1d(512)
+        self.l_bn3 = nn.BatchNorm1d(256)
+        self.l_bn4 = nn.BatchNorm1d(128)
+
+
+    def forward(self, latent_rep, xyz):
+        xyz = F.gelu(self.up_bn1(self.up1(xyz)))
+        xyz = self.up2(xyz)
+
+        x = torch.concat((latent_rep, xyz), dim = 1)
+
+        x = F.gelu(self.l_bn1(self.l1(x)))
+        x = F.gelu(self.l_bn2(self.l2(x)))
+        x = F.gelu(self.l_bn3(self.l3(x)))
+        x = F.gelu(self.l_bn4(self.l4(x)))
+        x = self.l5(x)
+
+        return x
+
+class SD_6L_Upscale256(nn.Module):
+
+    def __init__(self, latent_rep_size):
+        
+        super().__init__()
+
+        self.up1 = nn.Linear(3, 64)
+        self.up2 = nn.Linear(64, 128)
+        self.up3 = nn.Linear(128, latent_rep_size)
+
+        self.l1 = nn.Linear(latent_rep_size * 2, 2048)
+        self.l2 = nn.Linear(2048, 1024)
+        self.l3 = nn.Linear(1024, 512)
+        self.l4 = nn.Linear(512, 256)
+        self.l5 = nn.Linear(256, 64)
+        self.l6 = nn.Linear(64, 1)
+
+        self.up_bn1 = nn.BatchNorm1d(64)
+        self.up_bn2 = nn.BatchNorm1d(128)
+        self.up_bn3 = nn.BatchNorm1d(latent_rep_size)
+        
+        self.l_bn1 = nn.BatchNorm1d(2048)
+        self.l_bn2 = nn.BatchNorm1d(1024)
+        self.l_bn3 = nn.BatchNorm1d(512)
+        self.l_bn4 = nn.BatchNorm1d(256)
+        self.l_bn5 = nn.BatchNorm1d(64)
+
+
+    def forward(self, latent_rep, xyz):
+        xyz = F.gelu(self.up_bn1(self.up1(xyz)))
+        xyz = F.gelu(self.up_bn2(self.up2(xyz)))
+        xyz = self.up3(xyz)
+
+        x = torch.concat((latent_rep, xyz), dim = 1)
+
+        x = F.gelu(self.l_bn1(self.l1(x)))
+        x = F.gelu(self.l_bn2(self.l2(x)))
+        x = F.gelu(self.l_bn3(self.l3(x)))
+        x = F.gelu(self.l_bn4(self.l4(x)))
+        x = F.gelu(self.l_bn5(self.l5(x)))
+        x = self.l6(x)
+
+        return x
+    
+
+
+class SD_4L_Upscale256_LatentEncode(nn.Module):
+    def __init__(self, latent_dim, input_dim = 3, hidden_dim = 256):
+        super(SD_4L_Upscale256_LatentEncode, self).__init__()
+        self.x_fc = nn.Linear(input_dim, hidden_dim)
+        self.laten_fc = nn.Linear(latent_dim, hidden_dim) 
+        self.batch_norm1 = nn.BatchNorm1d(2* hidden_dim, affine=True)
+        self.fc1 = nn.Linear(2* hidden_dim, 2*hidden_dim)
+        self.batch_norm2 = nn.BatchNorm1d(2*hidden_dim, affine=True)
+        self.fc2 = nn.Linear(2*hidden_dim, hidden_dim)
+        self.batch_norm3 = nn.BatchNorm1d(hidden_dim, affine=True)
+        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
+        self.batch_norm4 = nn.BatchNorm1d(hidden_dim, affine=True)
+        self.fc4 = nn.Linear(hidden_dim, 1)
+        self.relu = nn.ReLU()
+
+
+    def forward(self, latent_encoding, x):
+        x = self.relu(self.x_fc(x))
+        latent_encoding = self.relu(self.laten_fc(latent_encoding))
+        x = torch.cat((x, latent_encoding), dim=-1)
+        x = self.batch_norm1(x)
+        x = self.batch_norm2(self.relu(self.fc1(x)))
+        x = self.batch_norm3(self.relu(self.fc2(x)))
+        x = self.batch_norm4(self.relu(self.fc3(x)))
+        x = self.fc4(x)
         return x
---
+++ SignDistanceModel/train.py
@@ -0,0 +1,202 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+import open3d as o3d
+from torch.utils.data import Dataset, DataLoader, Subset
+from dataclasses import dataclass
+import sys
+from pathlib import Path
+from tqdm import tqdm
+import sdf_models as sd
+import json
+
+
+sys.path.append(str(Path.cwd().parent))
+
+from Helpers.data import SDDataset
+import Helpers.PointCloudOpen3d as pc
+
+if torch.cuda.is_available():
+    device = "cuda"
+
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cpu"
+
+print(f'Using: {device}')
+
+
+# train_dataset = SDDataset('../Data/ModelNet40', '../Data/sampled_points', device, 1024, 'train', object_classes= None)
+# test_dataset = SDDataset('../Data/ModelNet40', '../Data/sampled_points', device, 1024, 'test', object_classes= None)
+
+# test_size = len(test_dataset)
+# split_idx = test_size // 2
+# indices = list(range(test_size))
+# val_dataset = Subset(test_dataset, indices[:split_idx])
+# test_dataset = Subset(test_dataset, indices[split_idx:])
+
+train_dataset = torch.load('../Data/sd_dataset_train.pt', weights_only= False)
+val_dataset = torch.load('../Data/sd_dataset_val.pt', weights_only= False)
+test_dataset = torch.load('../Data/sd_dataset_test.pt', weights_only= False)
+
+batch_size = 1024
+
+train_loader = DataLoader(train_dataset, batch_size= batch_size, shuffle= True)
+val_loader = DataLoader(val_dataset, batch_size= batch_size, shuffle= True)
+test_loader = DataLoader(test_dataset, batch_size= batch_size, shuffle= True)
+
+
+val_report_rate = 30000 # How often should we calculate the validation metrics.
+
+def calculate_acc(logits, targets): 
+    preds = torch.round(torch.sigmoid(logits))
+    correct = (preds == targets).float() 
+    accuracy = torch.sum(correct) / (correct.shape[0])
+    return accuracy.item()
+
+
+def calculate_val_metrics(model, device, val_loader):
+    # Calculate validation loss
+    val_loss = 0 
+    val_acc = 0
+
+    for data in val_loader:
+
+        latent_rep = data['latent_rep'].to(device)
+        xyz = data['xyz'].to(device)
+        sd = (data['sd'] >  0).float().to(device)
+
+        with torch.no_grad():
+            logits = model(latent_rep, xyz)
+            logits = logits.squeeze()
+
+            loss = F.binary_cross_entropy_with_logits(logits, sd)
+            val_loss += loss.item()
+            val_acc += calculate_acc(logits, sd)
+
+    val_loss /= len(val_loader)
+    val_acc /= len(val_loader)
+
+    return val_loss, val_acc
+
+def train_model(key, model, num_epochs, train_loader, val_loader, lr = 1e-3):
+
+    optimizer = optim.AdamW(model.parameters(), lr=lr)
+
+    min_val_loss = np.inf
+
+    train_losses = []
+    train_accs = []
+    val_losses = []
+    val_accs = []
+
+    for epoch in range(num_epochs):
+
+        # Train one epoch
+        model.train()
+
+        train_loss = 0 
+        train_acc = 0
+        
+        batch = 0
+        for data in tqdm(train_loader):
+ 
+            latent_rep = data['latent_rep'].to(device)
+            xyz = data['xyz'].to(device)
+            sd = (data['sd'] > 0).float().to(device)
+
+            logits = model(latent_rep, xyz)
+            logits = logits.squeeze()
+
+            optimizer.zero_grad()
+            
+            loss = F.binary_cross_entropy_with_logits(logits, sd)
+
+            loss.backward()
+
+            optimizer.step()
+
+            train_loss += loss.item()
+
+            # There are 120,000,000 examples per epoch, and so validation metrics should be calculated more frequently than 1 epoch
+            if batch % val_report_rate == 0:
+                model.eval()
+                val_loss, val_acc = calculate_val_metrics(model, device, val_loader)
+                model.train()
+
+                val_losses.append(val_loss)
+                val_accs.append(val_acc)
+
+                print(f'\nBatch Num {batch+1} \t Val Loss: {val_loss:.5f} \t Val Acc: {val_acc:.5f}')
+
+                # Save best model
+                if val_loss < min_val_loss:
+                    print(f'Val Loss Decreased({min_val_loss:.6f} ---> {val_loss:.6f}) \t Saving The Model')
+                    min_val_loss = val_loss
+
+                    torch.save(model.state_dict(), f'./trained_sdf_models/{key}')
+
+            batch+=1 
+
+
+        train_loss /= len(train_loader)
+        train_acc /= len(train_loader)
+
+        train_losses.append(train_loss)
+        train_accs.append(train_acc) 
+
+    model.eval()
+    val_loss, val_acc = calculate_val_metrics(model, device, val_loader)
+    val_losses.append(val_loss)
+    val_accs.append(val_acc)
+
+    results = {
+        "train_losses" : train_losses, 
+        "train_accs" : train_accs,
+        "val_losses" : val_losses, 
+        "val_accs" : val_accs
+    }
+
+    return results
+
+
+num_epochs = 1
+latent_size = 256
+lr = 1e-3
+
+overall_results = {}
+
+models = {
+    'SD_3L' : {'class' : sd.SD_3L , 'lr' : lr},
+    'SD_3L_Upscale32' : {'class' : sd.SD_3L_Upscale32 , 'lr' : lr},
+    'SD_7L_Upscale128' : {'class' : sd.SD_7L_Upscale128 , 'lr' : lr},
+    'SD_4L_Upscale128' : {'class' : sd.SD_4L_Upscale128 , 'lr' : lr},
+
+    'SD_3L_LR_1e-4' : {'class' : sd.SD_3L , 'lr' : 1e-4},
+    'SD_3L_Upscale32_LR_1e-4' : {'class' : sd.SD_3L_Upscale32 , 'lr' : 1e-4},
+    'SD_7L_Upscale128_LR_1e-4' : {'class' : sd.SD_7L_Upscale128 , 'lr' : 1e-4},
+    'SD_4L_Upscale128_LR_1e-4' : {'class' : sd.SD_4L_Upscale128 , 'lr' : 1e-4},
+
+    'SD_5L_Upscale256' : {'class' : sd.SD_5L_Upscale256 , 'lr' : lr},
+    'SD_6L_Upscale256' : {'class' : sd.SD_6L_Upscale256 , 'lr' : lr},
+    'SD_4L_Upscale256_LatentEncode' : {'class' : sd.SD_4L_Upscale256_LatentEncode , 'lr' : lr},
+}
+
+for key in models.keys():
+    print(f'Training {key} model')
+    
+    model = models[key]['class'](latent_size).to(device)
+
+    results = train_model(key, model, num_epochs, train_loader, val_loader, models[key]['lr'])
+
+    with open(f'./results/{key}.json', 'w') as f:
+        json.dump(results, f)
+
+    overall_results[key] = results
+
+with open(f'./results/overall_results.json', 'w') as f: 
+    json.dump(overall_results, f)
